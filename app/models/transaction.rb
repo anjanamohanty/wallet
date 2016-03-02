4672a5eb84129e4026dcd150e3b363b702a54697
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-03-02 18:24:07 -0500

    Refactor balance method for less lines of code

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,5 @@
 class Transaction < ActiveRecord::Base
   def self.balance
-    deposits = where("transaction_type == 0").sum(:amount)
-    withdrawals = where("transaction_type == 1").sum(:amount)
-    deposits - withdrawals
+    where("transaction_type == 0").sum(:amount) - where("transaction_type == 1").sum(:amount)
   end
 end
